cc10588ffc7836de379bb5c253858b93f69f48a8
Amateur-god <68581288+Amateur-God@users.noreply.github.com>
2023-08-09 19:02:21 +0100

    Delete .build directory

--- .build/@ONI Recon - Core/mod.cpp
+++
@@ -1,7 +0,0 @@
-name = "ONI Recon - Core";
-picture = "ONI_Recon.paa";
-hidePicture = "true";
-hideName = "false";
-actionName = "Discord";
-action = "https://discord.gg/9pJTHHzaFC";
-description = "Core Functionality for ONI Recon Unit";
