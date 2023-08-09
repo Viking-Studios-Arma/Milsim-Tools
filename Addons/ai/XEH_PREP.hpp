7b2d5f6ad2825b5b55d150fde4a512599951a188
Viking Studios <mrhowe95@gmail.com>
2023-08-09 02:13:36 +0100

    removed Populate AO from main branch until its working

--- Addons/ai/XEH_PREP.hpp
+++
@@ -1,8 +0,0 @@
-PREP(empty);
-PREP(3den_populateAO);
-PREP(findSpawnPos);
-PREP(getFactions);
-PREP(getRoadWaypoints);
-PREP(patrolRoad);
-PREP(populateAO);
-PREP(localityChangedEH);
--- Addons/main/functions/script_component.hpp
+++
@@ -1,1 +0,0 @@
-#include "x\VS_ONI_C\main\script_component.hpp"
