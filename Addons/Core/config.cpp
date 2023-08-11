604a550dc94c760bf00905716ebe6f11d7564d84
Viking Studios <mrhowe95@gmail.com>
2023-08-11 11:23:52 +0100

    Trying to get clientpreinit working

--- Addons/Core/config.cpp
+++ Addons/Core/config.cpp
@@ -66,6 +66,7 @@ class CfgFunctions {
 class Extended_PreInit_EventHandlers {
 	class oni_core_preInit {
 		init = "call compile preprocessFileLineNumbers '\x\vs_oni_c\core\XEH_preInit.sqf'";
+		init = "call compile preprocessFileLineNumbers '\x\vs_oni_c\core\XEH_clientPreInit.sqf'";
 	};
 };
 
